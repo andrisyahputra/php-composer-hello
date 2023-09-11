3029e976f335fb9b31ea111361ce26c6f7de3a56
andrisyahputra <andrisyahputra2209@gmail.com>
2023-09-11 11:41:14 +0700

    update versi 3

--- src/Customer.php
+++ src/Customer.php
@@ -7,7 +7,7 @@ class Customer
     public function __construct(private string $nama = "andrimc")
     {
     }
-    public function sayHello(string $nama): string
+    public function sayHello(string $nama = "andrimc"): string
     {
         return "Hello $nama, Perkanalkan nama saya adalah $this->nama";
     }
